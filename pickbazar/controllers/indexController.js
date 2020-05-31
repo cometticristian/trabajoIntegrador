85dbd13e6b4fe6a31659b720e6b2e0faac807354
DavidMessina <davidmessina9@gmail.com>
2020-05-31 14:33:34 -0300

    detail.ejs - index.ejs - list.ejs terminados

--- pickbazar/controllers/indexController.js
+++ pickbazar/controllers/indexController.js
@@ -5,8 +5,14 @@ const productsDB = path.join(__dirname, '../data/productsDB.json');
 let products = JSON.parse(fs.readFileSync(productsDB, 'utf-8'));
 
 const controller = {
-	root: (req, res) => {
-		res.render ('index', {products:products});
+	root: (req, res, next) => {
+		let selected = products.filter(product=>{
+			return product.important=="home";
+		});
+		let news = products.filter(product=>{
+			return product.important=="new";
+		});
+		res.render ('index', {category:products, selected, news});
     },
 }
     
--- pickbazar/controllers/productsController.js
+++ pickbazar/controllers/productsController.js
@@ -7,56 +7,70 @@ let products = JSON.parse(fs.readFileSync(productsDB, 'utf-8'));
 const controller = {
 	// Root - Show all products
 	root: (req, res, next) => {
-		res.render('./products/products', { products: products })
+		res.render('./products/list', { category: products, nombreCategoria: "Todos los productos" })
 	},
-
+	
 	category: (req, res, next) => {
-
 		let category = []
-		for (let i = 0; i < products.length; i++) {
-			if (products[i].category == req.params.productCategory) {
-				category.push(products[i])
+		products.forEach(function (product) {
+			if (product.category == req.params.productCategory) {
+				category.push(product)
 			}
-		}
-		res.render('./products/products', { category: category })
+		})
+		res.render('./products/list', { category: category, nombreCategoria: req.params.productCategory })
 	},
-
+	
 	subCategory: (req, res, next) => {
-
-		let category = []
-		for (let i = 0; i < products.length; i++) {
-			if (products[i].subCategory == req.params.productSubCategory) {
-				category.push(products[i])
+		let category = [];
+		let nombreCategoria = "";
+		products.forEach(function (product) {
+			if (product.subCategory == req.params.productSubCategory) {
+				category.push(product);
+				nombreCategoria = product.category
 			}
-		}
-		res.render('./products/products', { category: category })
+		})
+		res.render('./products/list', { category: category, nombreCategoria: nombreCategoria + " / " + req.params.productSubCategory })
 	},
-
+	
 	// Detail - Detail from one product
 	detail: (req, res, next) => {
-		res.render('./products/detail', {})
+		let product
+		for(let i=0; i<products.length; i++){
+			if(products[i].id == req.params.productId){
+				product = products[i];
+			}
+		}
+		let category = [];
+		products.forEach(function (similar) {
+			if (similar.category == product.category) {
+				category.push(similar);
+			}
+			
+		})
+		category=[category[0],category[1], category[2],category[3]];
+		res.render('./products/detail', {product:product, category:category})
 	},
-
+	
 	// Create - Form to create
 	create: (req, res, next) => {
 		res.render('./products/create-form')
 	},
-
+	
 	// Create -  Method to store
 	store: (req, res, next) => {
 		res.send("Producto cargado correctamente");
 	},
-
+	
 	// Update - Form to edit
 	edit: (req, res, next) => {
 		res.render("./products/edit-form", {})
 	},
-
+	
 	// Update - Method to update
 	update: (req, res, next) => {
 		res.redirect('./products/products')//detail/'+ parseInt(req.params.productId))
 	},
-
+	
 	// Delete - Delete one product from DB
 	destroy: (req, res, next) => {
 		res.redirect('./products/products');
@@ -66,4 +80,3 @@ const controller = {
 module.exports = controller;
 
 
- 
