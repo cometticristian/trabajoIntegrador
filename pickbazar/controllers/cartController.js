9f36ac42f89d3b1fe6c6a1e463d9091614f0b561
Cristian Cometti <cometticristian@gmail.com>
2020-07-19 10:26:17 -0300

    cart create finished

--- pickbazar/controllers/cartController.js
+++ pickbazar/controllers/cartController.js
@@ -11,77 +11,67 @@ const { Op } = require("sequelize");
 
 
 const controller = {
-       
+
     create: function (req, res, next) {
-        let productId = req.params.id;
-        var cartId;
-        db.Cart.findOne({where: {
-            user_id: req.session.userFound[0].id,
-            state: 1,
-        }})
-        .then((cart)=>{
-            if (cart) {
-                cartId = cart.id;
-            } else {
-                db.Cart.create({
+        console.log(req.session.userFound);
+
+        if (req.session.userFound == undefined) {
+            res.redirect('/users/login');
+        } else {
+            let productId = req.params.id;
+            var cartId;
+            db.Cart.findOne({
+                where: {
                     user_id: req.session.userFound[0].id,
-                    total: 0,
-                    state: 1
-                })
-                .then((cartCreated)=>{
-                    
-                    cartId = cartCreated.id;
-                })
-            }
-            
-            db.Product.findByPk(productId)
-            .then((product) => {
-                //console.log(product);
-                db.Cart_product.create({
-                    units: req.query.item,
-                    price: product.price,
-                    discount: product.discount,
-                    subtotal: 1,
-                    cart_id: cartId,
-                    product_id: productId,
-                })
+                    state: 1,
+                }
             })
-        })
-
-        /*db.Product.findByPk(productId,{})
-        
-
-        let cartProductReq = db.Cart_product.create({
-            units: req.query.item,
-            price: 1,
-            discount:10 ,
-            subtotal: 1,
-            cart_id:1,
-            product_id: productId,
-        })
-            .then((newCart) => {
-                console.log(newCart);
-
-                db.Cart_product.create({
-                    product_id: newProduct.id,
-                    cart_id: newCart.id,
-                    subtotal: 100, //req.price.value,
-                    units: 1,
-                    discount: idUser,
-                    date: new Date()
-                })
-            })
-            .then(() => {
-                res.redirect('/cart');
-            })
-            .catch((error) => {
-                console.log(error);
-            })
-
-*/
-        res.render('cart', { cart: cart });
-    },
+                .then((cart) => {
+                    if (cart) {
+                        cartId = cart.id;
     
+                        db.Product.findByPk(productId)
+                            .then((product) => {
+                                db.Cart_product.create({
+                                    units: req.query.item,
+                                    price: product.price,
+                                    discount: product.discount,
+                                    subtotal: 1,
+                                    cart_id: cartId,
+                                    product_id: productId,
+                                })
+                            })
+    
+                    } else {
+                        db.Cart.create({
+                            user_id: req.session.userFound[0].id,
+                            total: 0,
+                            state: 1
+                        })
+                            .then((cartCreated) => {
+                                cartId = cartCreated.id;
+    
+                                db.Product.findByPk(productId)
+                                    .then((product) => {
+                                        db.Cart_product.create({
+                                            units: req.query.item,
+                                            price: product.price,
+                                            discount: product.discount,
+                                            subtotal: 1,
+                                            cart_id: cartId,
+                                            product_id: productId,
+                                        })
+                                    })
+                            })
+                    }
+                })
+                .then(() => {
+                    res.redirect('/products');
+                })
+        }
+
+
+    },
 }
 
 module.exports = controller;
