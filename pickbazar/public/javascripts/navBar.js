01565d2421c8203b9ef3ab6810e23c3bfec1bb81
DavidMessina <davidmessina9@gmail.com>
2020-07-21 18:49:45 -0300

    Mobile Nav listo

--- pickbazar/public/javascripts/navBar.js
+++ pickbazar/public/javascripts/navBar.js
@@ -5,42 +5,40 @@ window.addEventListener('load', () => {
     let aCategory = document.querySelectorAll('.categoryTitle');
     let toggle = document.querySelector('.btn-toggle-navbar');
     let mobile = document.querySelector('.mobile-navbar');
-    
-    
-    if (mobile.style.display = 'none') {
-        toggle.addEventListener('click', () => {
+
+    let show = false
+    toggle.addEventListener('click', () => {
+        if (show == false) {
+            show = true
             mobile.style.display = 'block';
-            
-            if (mobile.style.display = 'block') {
-                toggle.addEventListener('click', () => {
-                    mobile.style.display = 'none';
-                })
-            }
-        })        
-    }
-    
+        } else if (show == true) {
+            show = false
+            mobile.style.display = 'none';
+        }
+    })
+
     for (let i = 0; i < aCategory.length; i++) {
         aCategory[i].addEventListener('mouseover', () => {
-        for (let pos=0; pos < ulSubcategory.length; pos++) {
+            for (let pos = 0; pos < ulSubcategory.length; pos++) {
                 ulSubcategory[i].style.display = 'block';
             }
         })
-    }   
+    }
     for (let i = 0; i < aCategory.length; i++) {
         aCategory[i].addEventListener('mouseout', () => {
-            for (let pos=0; pos < ulSubcategory.length; pos++) {
-                    ulSubcategory[i].style.display = 'none';
-            }    
-        })
-    }
-    for (let i=0; i< ulSubcategory.length; i++) {
-        ulSubcategory[i].addEventListener('mouseover', () => {   
-                ulSubcategory[i].style.display = 'block';
-        })
-    }
-    for (let i=0; i< ulSubcategory.length; i++) {
-        ulSubcategory[i].addEventListener('mouseout', () => {
+            for (let pos = 0; pos < ulSubcategory.length; pos++) {
                 ulSubcategory[i].style.display = 'none';
+            }
+        })
+    }
+    for (let i = 0; i < ulSubcategory.length; i++) {
+        ulSubcategory[i].addEventListener('mouseover', () => {
+            ulSubcategory[i].style.display = 'block';
+        })
+    }
+    for (let i = 0; i < ulSubcategory.length; i++) {
+        ulSubcategory[i].addEventListener('mouseout', () => {
+            ulSubcategory[i].style.display = 'none';
         })
     }
 })
