8f3ba33067b898c779ab569a4e3710429d997a79
unknown <davidmessina9@gmail.com>
2020-07-21 11:32:52 -0300

    mobile menu JS front

--- pickbazar/public/javascripts/navBar.js
+++ pickbazar/public/javascripts/navBar.js
@@ -1,12 +1,27 @@
 window.addEventListener('load', () => {
 
     let divUlSubcategory = document.querySelectorAll('.ulSubcategory')
-    let ulSubcategory = document.querySelectorAll('.generico');
+    let ulSubcategory = document.querySelectorAll('.subcategory');
     let aCategory = document.querySelectorAll('.categoryTitle');
-
+    let toggle = document.querySelector('.btn-toggle-navbar');
+    let mobile = document.querySelector('.mobile-navbar');
+    
+    
+    if (mobile.style.display = 'none') {
+        toggle.addEventListener('click', () => {
+            mobile.style.display = 'block';
+            
+            if (mobile.style.display = 'block') {
+                toggle.addEventListener('click', () => {
+                    mobile.style.display = 'none';
+                })
+            }
+        })        
+    }
+    
     for (let i = 0; i < aCategory.length; i++) {
         aCategory[i].addEventListener('mouseover', () => {
-            for (let pos=0; pos < ulSubcategory.length; pos++) {
+        for (let pos=0; pos < ulSubcategory.length; pos++) {
                 ulSubcategory[i].style.display = 'block';
             }
         })
